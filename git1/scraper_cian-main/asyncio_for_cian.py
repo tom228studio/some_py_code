ba1603639a21fe25d145225f2baf1730f5c8b4ce
Tom_ Studio <tom228studio@yandex.ru>
2022-01-15 09:37:17 +0300

    some new

--- git1/scraper_cian-main/asyncio_for_cian.py
+++ git1/scraper_cian-main/asyncio_for_cian.py
@@ -18,7 +18,7 @@ async def get_page_data(session, page):
         "user-agent": f"{ua.random}"
     }
 
-    url = f"https://www.cian.ru/cat.php?deal_type=sale&engine_version=2&offer_type=flat&p={page}&region=1&room1=1&room2=1"
+    url = f"https://www.cian.ru/cat.php?deal_type=sale&engine_version=2&offer_type=flat&p={page}&region=1&room1=1&room2=1#"
 
     async with session.get(url=url, headers=headers) as response:
         response_text = await response.text()
@@ -26,10 +26,15 @@ async def get_page_data(session, page):
         soup = BeautifulSoup(response_text, "lxml")
         try:
             for a in soup.find("div", attrs={"data-name": "Offers"}).find_all("a", class_=re.compile("--link--eoxce")):
-                print(a.get("href"))
+                print("{} ,{}".format(a.get("href"), page))
         except Exception as ex:
             print(ex)
-
+        try:
+            # Suggestions
+            for a in soup.find("div", attrs={"data-name": "Suggestions"}).find_all("a", class_=re.compile("--link--eoxce")):
+                print("new {}".format(a.get("href")))
+        except Exception as ex:
+                pass
 
 async def gather_data():
     headers = {
@@ -41,20 +46,9 @@ async def gather_data():
     async with aiohttp.ClientSession() as session:
         response = await session.get(url=url, headers=headers)
         soup = BeautifulSoup(await response.text(), "lxml")
-        page_count = int(soup.find("div", attrs={"data-name": "Pagination"}).find_all("li")[-2].text)
-        while True:
-            try:
-                old_page_cound = page_count
-                url = f"https://www.cian.ru/cat.php?deal_type=sale&engine_version=2&offer_type=flat&p={page_count}&region=1&room1=1&room2=1"
-                soup = BeautifulSoup(await response.text(), "lxml")
-                page_count = int(soup.find("div", attrs={"data-name": "Pagination"}).find_all("li")[-2].text)
-                if old_page_cound == page_count:
-                    page_count = int(soup.find("div", attrs={"data-name": "Pagination"}).find_all("li")[-1].text)
-                    break
-            except Exception as ex:
-                print(ex)
-                break
+        # page_count = int(soup.find("div", attrs={"data-name": "Pagination"}).find_all("li")[-2].text)
 
+        page_count = 54
 
         tasks = []
 
@@ -64,30 +58,16 @@ async def gather_data():
 
         await asyncio.gather(*tasks)
 
-# async def check_new_page(page):
-#     headers = {
-#         "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-#         "user-agent": f"{ua.random}"
-#     }
-#
-#     url = f"https://www.cian.ru/cat.php?deal_type=sale&engine_version=2&offer_type=flat&p={page}&region=1&room1=1&room2=1"
-#     async with aiohttp.ClientSession() as session:
-#          response = await session.get(url=url, headers=headers)
-#          soup = BeautifulSoup(await response.text(), "lxml")
-#          page_count = int(soup.find("div", attrs={"data-name": "Pagination"}).find_all("li")[-2].text)
-#          print(page_count)
-#          tasks = []
-#
-#          for page in range(page, page_count + 1):
-#              task = asyncio.create_task(get_page_data(session, page))
-#              tasks.append(task)
-#          await asyncio.gather(*tasks)
-
 
 def main():
     asyncio.run(gather_data())
-    print(time.time() - start_time)
+    print(f"54 page {time.time() - start_time} second")
 
 
 if __name__ == "__main__":
     main()
+
+# https://www.cian.ru/cat.php?deal_type=sale&engine_version=2&offer_type=flat&p=54&region=1&room1=1&room2=1&room3=1
+# https://www.cian.ru/cat.php?deal_type=sale&engine_version=2&offer_type=flat&p=54&region=1&room1=1&room2=1&room3=1#
+# response.json()
+# реализовать постоянную подгрузку с проверкой на схожость
